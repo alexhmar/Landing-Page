70b9a86f7dc5c251aefc61119b19a84f1efe346f
Alex Hmar <alexnsanate@gmail.com>
2021-11-30 16:10:28 +0530

    revert route path

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="Landing-Page/thankyou" element={<ThankYou />} />
+          <Route path="/thankyou" element={<ThankYou />} />
           <Route
-            path="Landing-Page/"
+            path="/"
             element={
               <div className="Container">
                 <SignUp validate={validate} />
@@ -22,7 +22,7 @@ function App() {
               </div>
             }
           />
-          <Route path="Landing-Page/about" element={<About />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Router>
     </div>
